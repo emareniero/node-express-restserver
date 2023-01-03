3c06d13cd5dfb1ac1c32462263492f70294e5ecc
Emanuel Reniero <emareniero@gmail.com>
2023-01-03 09:45:27 +0100

    Added configurations

--- database/config.js
+++ database/config.js
@@ -5,7 +5,11 @@ dotenv.config({ path: "./.env" });
 const dbConnection = async () => {
   try {
 
-    await mongoose.connect(process.env.MONGODB_CNN); // Dejamos el await para esperar que la conexion se haga y si no se hace arroje el error!
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology:true,
+      useCreateIndex: true
+    }); // Dejamos el await para esperar que la conexion se haga y si no se hace arroje el error!
 
     console.log('Base de datos online!');
 
